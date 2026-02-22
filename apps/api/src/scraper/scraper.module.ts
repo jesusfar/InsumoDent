bf1f8b542c2c29f5d50ce2a48fdba20144b55ef9
jesusfar <jesusfar100@gmail.com>
2026-02-22 02:55:33 -0300

    fix(scrapers): build configuration and nestjs workers injection

--- apps/api/src/scraper/scraper.module.ts
+++ apps/api/src/scraper/scraper.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bullmq';
 import { ScraperController } from './scraper.controller';
 import { ScraperService } from './scraper.service';
+import { ScraperProcessor } from './scraper.processor';
 
 @Module({
+    imports: [
+        BullModule.registerQueue({
+            name: 'scraping-queue',
+        }),
+    ],
     controllers: [ScraperController],
-    providers: [ScraperService],
+    providers: [ScraperService, ScraperProcessor],
     exports: [ScraperService],
 })
 export class ScraperModule { }
